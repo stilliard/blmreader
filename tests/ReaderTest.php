741bddc4b1d5e082be3612d35dc97888d555ab7f
Andrew Stilliard <andrew.stilliard@gmail.com>
2017-03-23 00:57:36 +0000

    Fixed travis CI new phpunit version

--- tests/ReaderTest.php
+++ tests/ReaderTest.php
@@ -2,6 +2,12 @@
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
+// backward compatibility
+if (!class_exists('\PHPUnit_Framework_TestCase') &&
+    class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
 class ReaderTest extends PHPUnit_Framework_TestCase
 {
     public function testToArray()
